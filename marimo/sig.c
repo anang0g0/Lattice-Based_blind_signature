6b5ca8c8278cbcfdd6dc528cf96f726c425a8614
Osamu Sakai <fumumue@protonmail.com>
2024-01-31 00:19:14 +0900

    bourne2buggy

--- marimo/sig.c
+++ marimo/sig.c
@@ -47,29 +47,119 @@ long long sum(short a[N]){
     return u;
 }
 
+void idm(){
+    short z[N]={0};
+    short v[N]={0};
+    short ra[N]={0};
+    short w[N]={0};
+
+    int i, j, k;
+    short P[N] = {0},Pa[N]={0};
+    short b = 0;
+    int a2[32][64] = {0};
+    int a3[32][64] = {0};
+
+
+    for (i = 0; i < N; i++){
+        P[i] = i;
+        Pa[i]=i;
+    }
+    for (i = 0; i < N; i++)
+        P[i] = i;    
+    random_shuffle(P, N);
+    random_shuffle(Pa,N);
+    short inv_P[N];
+    short inv_Pa[N]={0};
+    for (i = 0; i < N; i++)
+    {
+        printf("%d,", P[i]);
+        inv_P[P[i]] = i;
+    }
+
+    for(i=0;i<N;i++){
+    ra[i]=rand()%4096;
+    w[i]=rand()%4096;
+    if(rand()%2==1)
+    ra[i]= -1*ra[i];
+    if(rand()%2==1)
+    v[i]= -1*w[inv_Pa[i]];
+    }
+    for(i=0;i<N;i++)
+    inv_Pa[Pa[i]]=i;
+    printf("\n");
+
+
+    short p1[N]={0},p2[N]={0},p3[N]={0};
+    int ca;
+    
+    for(j=0;j<1;j++){    
+    ca=rand()%4093;
+    for(int kk=0;kk<1;kk++)
+    {
+    for(i=0;i<N;i++){
+    z[i]=ra[P[i]]+ca*v[Pa[P[i]]];
+    //printf("aa%d %d\n",z[i],p3[i]);
+    }
+
+    for(i=0;i<N;i++){
+    p1[i]=ca*v[Pa[i]];
+    p2[i]=ra[P[i]];
+    p3[i]=ca*v[Pa[P[i]]];
+    //z[i]=ca*v[Pa[i]];
+    }
+    //for(i=0;i<N;i++)
+    //printf("%d %d\n",p3[i],p2[i]);
+    long long c0=sum(p3);
+    long long c1=sum(p2);
+    printf("%lld\n",c0);
+    printf("%lld\n",c1);
+b=rand()%2;
+short tmp[N]={0};
+if(b==0)
+{
+    for(i=0;i<N;i++)
+    tmp[i]=z[inv_P[i]];
+    if(sum(ra)==(sum(tmp)-sum(p1))){
+    printf("That's True\n");
+    }else{
+        printf("baka1\n");
+        exit(1);
+    }
+}
+if(b==1){
+    if(sum(p2)==(sum(z)-sum(p3)))
+    {
+        printf("That's True!\n");
+    }else{
+        printf("baka2\n");
+        exit(1);
+    }
+}
+    }
+    printf("::\n");
+    }
+
+
+}
+
+
 int main()
 {
-    short rr[N][N] = {0};
-    short ss[N][N] = {0};
-    short tt[N][N] = {0};
-    short ga[N][N] = {0};
     short z[17][17][N]={0};
     short v[N]={0};
     short ra[N]={0};
     short w[N]={0};
 
     int i, j, k;
-    short c[N][N] = {0};
-    short d[K][K] = {0};
     short P[N] = {0},Pa[N]={0};
-    short bb[N][N] = {0};
-    short cc[K][N] = {0};
-    short cd[N][N] = {0};
     short b[N][N] = {0};
     int a2[32][64] = {0};
     int a3[32][64] = {0};
 
     srand(clock());
+
+    idm();
+    exit(1);
 
     for (i = 0; i < N; i++){
         P[i] = i;
